4202f683b1f2cc0740710e23eb4bb65a6317dc3a
Unix-User <wevertonslima@gmail.com>
2022-06-03 15:32:06 -0300

    Alterações em contratos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,29 +16,134 @@ use MercadoPago\SDK;
 use MercadoPago\Payment;
 use MercadoPago\Preference;
 use MercadoPago\Item;
+use MercadoPago\Invoice;
+use MercadoPago\Plan;
+use MercadoPago\Subscription;
+use MercadoPago\MerchantOrder;
+
 
 class UserController extends Controller
 {
 
     public function index()
     {
-        $users = User::paginate(10);
-        return view('users.index', compact('users'));
+        if (auth()->user()->teams->first()->id == '1') {
+            $users = User::paginate(10);
+            return view('users.index', compact('users'));
+        }
+        return redirect('/products');
     }
 
     public function create()
     {
-        $products = Product::all();
-        $teams = Team::all();
-        return view('users.create', compact('products', 'teams'));
+        if (!Auth::check() || auth()->user()->id == 1) {
+            $products = Product::all();
+            return view('users.create', compact('products'));
+        } elseif (auth()->user()->teams->first()->id == 1) {
+            $products = Product::where('user_id', auth()->user()->id)->get();
+            return view('users.create', compact('products'));
+        } else {
+            return redirect('/products');
+        }
     }
 
     public function store(Request $request)
     {
+        if (!Auth::check() || (auth()->user()->teams->first()->id == ('1' || '2'))) {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+                'phone' => 'required|string|max:255|unique:users',
+                'cep' => 'string|max:255',
+                'rua' => 'string|max:255',
+                'num' => 'string|max:255',
+                'bairro' => 'string|max:255',
+                'cidade' => 'string|max:255',
+                'uf' => 'string|max:255',
+                'url' => 'string|max:255',
+                'product_id' => 'required',
+                'api_key' => 'string|max:255',
+                'expires_at' => 'string|max:255'
+            ]);
+
+            $user = new User;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->cep = $request->cep;
+            $user->rua = $request->rua;
+            $user->num = $request->num;
+            $user->etc = $request->etc;
+            $user->bairro = $request->bairro;
+            $user->cidade = $request->cidade;
+            $user->uf = $request->uf;
+            $user->url = $request->url;
+            $user->api_key = $request->api_key;
+            $user->expires_at = $request->expires_at;
+            $user->password = bcrypt($request->password);
+            $product = Product::where('id', $request->product_id)->first();
+            if (!$product) {
+                return redirect('/product/create')->with('error', 'Cadastre um produto primeiro!');
+            }
+            $team = Team::where('id', $product->user->id)->first();
+            if (!$team) {
+                $team = new Team;
+                $team->name = $product->user->name;
+                $team->save();
+            }
+            $user->save();
+            $user->teams()->sync($team, ['created_at' => now()]);
+            $user->contracts()->sync($request->product_id, ['created_at' => now()]);
+            return redirect('/users')->with('success', 'User created successfully');
+        }
+        return redirect('/users')->with('error', 'You are not authorized to create users');
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        switch (auth()->user()->id) {
+            case auth()->user()->id == '1':
+                return view('users.show', compact('user'));
+            case auth()->user()->id == $user->teams->first()->id:
+                return view('users.show', compact('user'));
+            case auth()->user()->id == $user->id:
+                return view('users.show', compact('user'));
+            default:
+                return redirect('/users');
+        }
+    }
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+        /* 
+         * admin team is id 1
+         * test if user is admin or user is editing his own profile
+         */
+        switch (auth()->user()->id) {
+            case auth()->user()->id == $user->id:
+                $products = Product::where('user_id', auth()->user()->teams->first()->id)->get();
+                return view('users.edit', compact('user', 'products'));
+            case auth()->user()->id == '1':
+                $products = Product::all();
+                return view('users.edit', compact('user', 'products'));
+            case auth()->user()->id == $user->teams->first()->id:
+                $products = Product::where('user_id', auth()->user()->id)->get();
+                return view('users.edit', compact('user', 'products'));
+            default:
+                return redirect('/users');
+        }
+        return redirect('/users')->with('error', 'You are not authorized to edit this user');
+    }
+
+    public function update(Request $request, $id)
+    {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'phone' => 'required|string|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
+            'phone' => 'required|string|max:255|unique:users,phone,' . $id,
             'cep' => 'string|max:255',
             'rua' => 'string|max:255',
             'num' => 'string|max:255',
@@ -50,8 +155,7 @@ class UserController extends Controller
             'api_key' => 'string|max:255',
             'expires_at' => 'string|max:255'
         ]);
-
-        $user = new User;
+        $user = User::find($id);
         $user->name = $request->name;
         $user->email = $request->email;
         $user->phone = $request->phone;
@@ -65,95 +169,33 @@ class UserController extends Controller
         $user->url = $request->url;
         $user->api_key = $request->api_key;
         $user->expires_at = $request->expires_at;
-        if(!empty($request->password)) {
-            $request->validate([
-                'password' => 'required|string|min:6'
-            ]);
+        if ($request->password) {
             $user->password = bcrypt($request->password);
         }
-        if (Product::count() > 0) {
-            $team = Product::find($request->product_id)->first()->tags;
-        } else {
-            if (User::count() == 0) {
-                $team = 1;
-            } else {
-                return redirect('/products')->with('error', 'Não existem produtos cadastrados');
-            }
+        $product = Product::where('id', $request->product_id)->first();
+        if (!$product) {
+            return redirect('/product/create')->with('error', 'Cadastre um produto primeiro!');
+        }
+        $team = Team::where('id', $product->user->id)->first();
+        if (!$team) {
+            $team = new Team;
+            $team->name = $product->user->name;
+            $team->save();
         }
         $user->save();
-        $user->teams()->sync($team);
-        $user->contracts()->sync($request->product_id);
-
-        return redirect('/users')->with('success', 'User created successfully');
-    }
-
-    public function show($id)
-    {
-        $user = User::find($id);
-        return view('users.show', compact('user'));
-    }
-
-    public function edit($id)
-    {
-        $user = User::find($id);
-        $products = Product::all();
-        return view('users.edit', compact('user', 'products'));
-    }
-
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
-            'password' => 'required|string|min:6',
-            'phone' => 'required|string|max:255|unique:users,phone,' . $id,
-            'cep' => 'max:255',
-            'rua' => 'string|max:255',
-            'num' => 'string|max:255',
-            'etc' => 'string|max:255',
-            'bairro' => 'string|max:255',
-            'cidade' => 'string|max:255',
-            'uf' => 'string|min:2|max:2',
-            'url' => 'string|max:255',
-            'api_key' => 'string|max:255',
-            'expires_at' => 'string|max:255',
-        ]);
-        $user = User::find($id);
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->phone = $request->phone;
-        $user->cep = $request->cep;
-        $user->rua = $request->rua;
-        $user->num = $request->num;
-        $user->etc = $request->etc;
-        $user->bairro = $request->bairro;
-        $user->cidade = $request->cidade;
-        $user->uf = $request->uf;
-        $user->url = $request->url;
-        $user->api_key = $request->api_key;
-        $user->expires_at = $request->expires_at;
-        $user->password = bcrypt($request->password);
-        if (Product::count() > 0) {
-            $team = Product::find($request->product_id)->first()->tags;
-        } else {
-            if (User::count() == 0) {
-                $team = 1;
-            } else {
-                return redirect('/products')->with('error', 'Não existem produtos cadastrados');
-            }
-        }
-        $user->save();
-        $user->teams()->sync($team);
-        $user->contracts()->sync($request->product_id);
-
+        $user->teams()->sync($team, ['updated_at' => now()]);
+        $user->contracts()->attach($request->product_id, ['reference' => now(), 'created_at' => now()]);
         return redirect('/users')->with('success', 'User updated successfully');
     }
 
     public function delete($id)
     {
         $user = User::find($id);
-        $user->delete();
-        return redirect('/users')->with('success', 'User deleted successfully');
+        if (auth()->user()->id == $user->id || auth()->user()->id == '1' || (auth()->user()->teams->first()->id == '1' && $user->teams->first()->id == auth()->user()->id)) {
+            $user->delete();
+            return redirect('/users')->with('success', 'User deleted successfully');
+        }
+        return redirect('/users')->with('error', 'You are not authorized to delete users');
     }
 
     public function login()
@@ -168,7 +210,7 @@ class UserController extends Controller
             'password' => ['required'],
         ]);
         if (Auth::attempt($credentials)) {
-            return redirect('/products');
+            return redirect('/user/' . Auth::user()->id . '/show');
         } else {
             return redirect('/login')->with('error', 'Invalid credentials');
         }
@@ -224,27 +266,27 @@ class UserController extends Controller
 
     public function payment($id)
     {
+
         $user = User::find($id);
-        SDK::setAccessToken(env('MP_ACCESS_TOKEN'));
+        SDK::setAccessToken(env('Test_MP_ACCESS_TOKEN'));
         $preference = new Preference();
         $item = new Item();
-        $item->title = $user->contracts->first()->name;
+        $item->title = $user->contracts->last()->name;
         $item->quantity = 1;
-        $item->unit_price = $user->contracts->first()->price;
+        $item->unit_price = $user->contracts->last()->price;
         $preference->items = array($item);
+        $preference->external_reference = $user->contracts->last()->id;
         $preference->save();
+
         $payment = new Payment();
-        $payment->transaction_amount = $user->contracts->first()->price;
-        $payment->description = $user->contracts->first()->name;
+        $payment->transaction_amount = $user->contracts->last()->price;
+        $payment->description = $user->contracts->last()->name;
         $payment->payment_method_id = "pix";
+        $payment->external_reference = $user->contracts->last()->id;
         $payment->payer = array(
-            "email" => "wevertonsilveiralima@hotmail.com",
+            "email" => $user->email,
             "first_name" => $user->name,
             "last_name" => $user->name,
-            "identification" => array(
-                "type" => "CPF",
-                "number" => "19119119100"
-            ),
             "address" =>  array(
                 "zip_code" => $user->cep,
                 "street_name" => $user->rua,
@@ -255,7 +297,50 @@ class UserController extends Controller
             )
         );
         $payment->save();
-        return view('users.checkout', compact('user', 'payment', 'preference'));
+
+        $log = file_get_contents(public_path() . '/log.txt');
+
+        return view('users.checkout', compact('user', 'payment', 'preference', 'log'));
+    }
+
+    public function processar_pagamento(Request $request)
+    {
+
+        $log = file_get_contents(public_path() . '/log.txt');
+        $log .= "\n" . date('Y-m-d H:i:s') . " - Nova requisição para processar pagamento";
+        file_put_contents(public_path() . '/log2.txt', $request);
+
+        SDK::setAccessToken(env('Test_MP_ACCESS_TOKEN'));
+        switch($request->type) {
+            case "payment":
+                $payment = Payment::find_by_id($request->data->id);
+                $log .= "\n" . date('Y-m-d H:i:s') . " - Pagamento encontrado";
+                if ($payment->status == "approved") {
+                    $log .= "\n" . "Pagamento aprovado";
+                    $contract = Contract::find($payment->external_reference);
+                    $contract->sync([
+                        'reference' => 'paid',
+                        'updated_at' => now()
+                    ]);
+                    $log .= "\n" . "Sistema liberado";
+                } else {
+                    $log .= "\n" . "Pagamento não aprovado";
+                }
+                break;
+            case "plan":
+                $plan = Plan::find_by_id($request->data->id);
+                break;
+            case "subscription":
+                $plan = Subscription::find_by_id($request->data->id);
+                break;
+            case "invoice":
+                $plan = Invoice::find_by_id($request->data->id);
+                break;
+            case "point_integration_wh":
+                // $_POST contém as informações relacionadas à notificação.
+                break; 
+        }
+        file_put_contents(public_path() . '/log.txt', $log);
     }
 
     public function logout()
