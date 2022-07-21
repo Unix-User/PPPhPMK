a2ee62c3bc3d1071644498489583e94a8a96f83b
Unix-User <wevertonslima@gmail.com>
2022-07-21 18:05:16 -0300

    fix some bugs

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -35,15 +35,12 @@
                         <a href="#">{{ date('d/m/Y H:i:s') }}</a>
                     </li>
                     <li><a href="/products">Produtos</a></li>
-                    @if (Auth::user()->teams->first()->id == '1')
-                    <li><a href="/users">Pessoas</a></li>
-                    @endif
                     <li class="dropdown">
                         <a href="#" class="text-capitalize">{{ Auth::user()->name }}</a>
                         <ul>
                             <li><a href="/user/{{ Auth::id() }}/show ">Perfil</a></li>
                             @if (Auth::user()->teams->first()->id == '1')
-                            <li><a href="/users ">Usuários</a></li>
+                            <li><a href="/users ">Clientes</a></li>
                             <li><a href="/devices">Dispositívos</a></li>
                             <li><a href="/system">Sistema</a></li>
                             @endif
