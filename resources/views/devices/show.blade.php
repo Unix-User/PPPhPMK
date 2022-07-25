abb2ca2839faaeec0f1dce6c4dbbf9c873866cfb
Unix-User <wevertonslima@gmail.com>
2022-07-25 13:17:21 -0300

    some fixes and scheduler for device sincronization

--- resources/views/devices/show.blade.php
+++ resources/views/devices/show.blade.php
@@ -28,20 +28,28 @@
             <tbody>
                 @foreach ($newUsers as $active)
                 <tr>
-                    <th><a class="clean" href="/user/{{ $active->id }}/show">{{ $active->name }}</a></th>
-                    <th>{{ (isset($active->profile)) ? (($active->contracts->last()->product->tags == null) ? 'default' : $active->contracts->last()->product->tags . "m" ) : "Não registrado" ; }}</th>
-                    <th>{{ $active->uptime }}</th>
-                    <th><a href="http://{{ $active->address }}" target="_blank" class="icon solid fa-link" data-toggle="tooltip" title="Acessar antena/roteador"> {{ $active->address }}</a></th>
-                    <th>{{ $active->mac }}</th>
-                    <th class="text-right">
-                        @if ($active->status == "false")
-                        <a href="/user/{{ $active->name }}/disable" class="icon solid fa-unlock" data-toggle="tooltip" title="Bloquear usuário"></a>
+                    @php
+                    $d1 = strtotime($active->contracts->last()->updated_at);
+                    $d2 = ceil(($d1 - time()) / 60 / 60 / 24);
+                    @endphp
+
+                    @if ($d2 + 30 < 1) <th><a class="clean text-warning" href="/user/{{ $active->id }}/show">{{ $active->name }}</a></th>
                         @else
-                        <a href="/user/{{ $active->name }}/enable" class="icon solid fa-lock" data-toggle="tooltip" title="Desbloquear usuário"></a>
+                        <th><a class="clean" href="/user/{{ $active->id }}/show">{{ $active->name }}</a></th>
                         @endif
-                        <a href="/user/{{ $active->name }}/disconnect" class="icon solid fa-wifi" data-toggle="tooltip" title="Desconectar usuário de {{ $device->name }}"></a>
-                        <a href="/user/{{ $active->name }}/remove" class="icon solid fa-trash" data-toggle="tooltip" title="Remover usuário de {{ $device->name }}"></a>
-                    </th>
+                        <th>{{ (isset($active->profile)) ? (($active->contracts->last()->product->tags == null) ? 'default' : $active->contracts->last()->product->tags . "m" ) : "Não registrado" ; }}</th>
+                        <th>{{ $active->uptime }}</th>
+                        <th><a href="http://{{ $active->address }}" target="_blank" class="icon solid fa-link" data-toggle="tooltip" title="Acessar antena/roteador"> {{ $active->address }}</a></th>
+                        <th>{{ $active->mac }}</th>
+                        <th class="text-right">
+                            @if ($active->status == "false")
+                            <a href="/user/{{ $active->name }}/disable" class="icon solid fa-unlock" data-toggle="tooltip" title="Bloquear usuário"></a>
+                            @else
+                            <a href="/user/{{ $active->name }}/enable" class="icon solid fa-lock" data-toggle="tooltip" title="Desbloquear usuário"></a>
+                            @endif
+                            <a href="/user/{{ $active->name }}/disconnect" class="icon solid fa-wifi" data-toggle="tooltip" title="Desconectar usuário de {{ $device->name }}"></a>
+                            <a href="/user/{{ $active->name }}/remove" class="icon solid fa-trash" data-toggle="tooltip" title="Remover usuário de {{ $device->name }}"></a>
+                        </th>
                 </tr>
                 @endforeach
             </tbody>
